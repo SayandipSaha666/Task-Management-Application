0cdf1f5e7d2b97f8618698ae35c4ad70483c7fb9
SayandipSaha666 <sahasayandip480@gmail.com>
2026-04-03 11:29:52 +0530

    changes in db connection

--- Server/database/database.js
+++ Server/database/database.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose')
-//  process.env.MONGO_URI // For cloud server
-// let URI = "mongodb://localhost:27017/Task_Management_App"
-// let URI = "mongodb+srv://noob_coder_27:Saha%4006122004@cluster0.xhkc6lj.mongodb.net/Taskflow_DB"
-// URI = "mongodb://mongodb:27017/Task_Management_App" // While running the server on Docker -> mongo instance also running in docker
-// So don't use localhost then instead use image name
+
 const URI = process.env.MONGO_URI()
 const connectDB = async()=>{
     try {
